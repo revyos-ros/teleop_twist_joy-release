1d644a5e07da29e8717a5e08611e81f4f47b3798
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-09-23 22:59:32 +0800

    Rebase from 'debian/humble/teleop_twist_joy'

--- launch/teleop-launch.py
+++ launch/teleop-launch.py
@@ -9,12 +9,14 @@ import launch_ros.actions
 def generate_launch_description():
     joy_config = launch.substitutions.LaunchConfiguration('joy_config')
     joy_dev = launch.substitutions.LaunchConfiguration('joy_dev')
+    publish_stamped_twist = launch.substitutions.LaunchConfiguration('publish_stamped_twist')
     config_filepath = launch.substitutions.LaunchConfiguration('config_filepath')
 
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument('joy_vel', default_value='cmd_vel'),
         launch.actions.DeclareLaunchArgument('joy_config', default_value='ps3'),
-        launch.actions.DeclareLaunchArgument('joy_dev', default_value='/dev/input/js0'),
+        launch.actions.DeclareLaunchArgument('joy_dev', default_value='0'),
+        launch.actions.DeclareLaunchArgument('publish_stamped_twist', default_value='false'),
         launch.actions.DeclareLaunchArgument('config_filepath', default_value=[
             launch.substitutions.TextSubstitution(text=os.path.join(
                 get_package_share_directory('teleop_twist_joy'), 'config', '')),
@@ -23,13 +25,14 @@ def generate_launch_description():
         launch_ros.actions.Node(
             package='joy', executable='joy_node', name='joy_node',
             parameters=[{
-                'dev': joy_dev,
+                'device_id': joy_dev,
                 'deadzone': 0.3,
                 'autorepeat_rate': 20.0,
             }]),
         launch_ros.actions.Node(
             package='teleop_twist_joy', executable='teleop_node',
-            name='teleop_twist_joy_node', parameters=[config_filepath],
+            name='teleop_twist_joy_node',
+            parameters=[config_filepath, {'publish_stamped_twist': publish_stamped_twist}],
             remappings={('/cmd_vel', launch.substitutions.LaunchConfiguration('joy_vel'))},
             ),
     ])
